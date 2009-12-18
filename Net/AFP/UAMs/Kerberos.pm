92f86dc723951d32b658431833ed5c4f526948bd
demon <demon@f17517cd-6fe8-449b-99a5-82cbd33fd24b>
2009-12-18 15:04:17 +0000

    First cut at a Kerberos client UAM. It compiles, and pretty much follows Apple's description of the UAM's operation, but unfortunately without a test environment (which I think requires OS X Server - I don't think Kerberized AFP is available in the normal AFP server component of OS X), I can't really verify its operation at all.

--- Net/AFP/UAMs/Kerberos.pm
+++ Net/AFP/UAMs/Kerberos.pm
@@ -4,6 +4,7 @@ use constant UAMNAME => 'Client Krb v2';
 
 use GSSAPI;
 use Net::AFP::Result;
+use Net::AFP::Connection;
 
 use strict;
 
@@ -23,18 +24,42 @@ sub Authenticate {
 	# we attempt to do FPLoginExt()...
 
 	my $resp = '';
-	my $rc = $session->FPLoginExt(0, $AFPVersion, UAMNAME, 3, $username,
-			3, $realm, undef, \$resp);
+	my $rc = $session->FPLoginExt(0, $AFPVersion, UAMNAME, kFPUTF8Name,
+			$username, kFPUTF8Name, $realm, undef, \$resp);
 	print 'FPLoginExt() completed with result code ', $rc, "\n"
 			if defined $::__AFP_DEBUG;
 
-	return $rc unless $rc == Net::AFP::Result::kFPAuthContinue;
+	return $rc unless $rc == kFPAuthContinue;
 
-	if (1) { # using Kerberos v5
-		my $message = pack('C/a*x![s]n/a*', $username, $ticket);
+	my $ctx;
+	my $gss_input_token = q();
+	my $status = GSSAPI::Context::init($ctx, GSS_C_NO_CREDENTIAL,
+			$realm, GSS_C_NO_OID, GSS_C_MUTUAL_FLAG | GSS_C_REPLAY_FLAG,
+			0, GSS_C_NO_CHANNEL_BINDINGS, $gss_input_token, my $out_mech,
+			my $gss_output_token, my $out_flags, my $out_time);
 
-	# FIXME: Need to initialize the security context; don't yet know how to
-	# do that though, as I've never done Kerberos/GSSAPI programming.
+	$status || gss_exit("CLIENT::Unable to initialize security context", $status);
+
+	my $message = pack('C/a*x![s]n/a*', $username, $gss_output_token);
+	my $sresp;
+	$rc = $session->FPLoginCont($$resp{'ID'}, $message, \$sresp);
+
+	return $rc unless $rc == kFPNoErr;
+
+	($gss_input_token) = unpack('n/a*', $$sresp{'UserAuthInfo'});
+
+	$status = GSSAPI::Context::init($ctx, GSS_C_NO_CREDENTIAL,
+			$realm, GSS_C_NO_OID, GSS_C_MUTUAL_FLAG | GSS_C_REPLAY_FLAG,
+			0, GSS_C_NO_CHANNEL_BINDINGS, $gss_input_token, $out_mech,
+			$gss_output_token, $out_flags, $out_time);
+
+	$status || gss_exit("CLIENT::Unable to initialize security context", $status);
+
+	# FIXME: Apple's docs talk about using FPGetSessionToken() about
+	# here to get a random session key from the server, and also something
+	# about a disconnect token; I'm not really clear here on what I'm
+	# supposed to do with all that.
+	return $rc;
 
 }
 die('Incomplete code, should not be used yet');
