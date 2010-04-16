b0fba45a482d58ad60f69555c4e6856aefdb26b4
demon <demon@f17517cd-6fe8-449b-99a5-82cbd33fd24b>
2010-04-16 15:31:33 +0000

    Updates.

--- Net/Atalk.pm
+++ Net/Atalk.pm
@@ -6,10 +6,20 @@ use strict;
 use warnings;
 
 our @ISA = qw(Exporter);
-our @EXPORT = qw(atalk_aton atalk_ntoa pack_sockaddr_at unpack_sockaddr_at);
+our @EXPORT = qw(atalk_aton atalk_ntoa pack_sockaddr_at unpack_sockaddr_at
+				 DDPTYPE_RTMPRD DDPTYPE_NBP DDPTYPE_ATP DDPTYPE_AEP
+				 DDPTYPE_RTMPR DDPTYPE_ZIP DDPTYPE_ADSP );
 
 sub AF_APPLETALK { return 5; }
 sub PF_APPLETALK { return AF_APPLETALK; }
+
+use constant DDPTYPE_RTMPRD	=> 1;
+use constant DDPTYPE_NBP	=> 2;
+use constant DDPTYPE_ATP	=> 3;
+use constant DDPTYPE_AEP	=> 4;
+use constant DDPTYPE_RTMPR	=> 5;
+use constant DDPTYPE_ZIP	=> 6;
+use constant DDPTYPE_ADSP	=> 7;
 
 sub atalk_aton {
 	my($addr) = @_;
@@ -36,7 +46,7 @@ sub sockaddr_at {
 sub pack_sockaddr_at {
 	my($port, $paddr) = @_;
 
-	return pack('nCa[3]x[8]', AF_APPLETALK, $port, $paddr);
+	return pack('nCxa[3]x[8]', AF_APPLETALK, $port, $paddr);
 }
 
 sub unpack_sockaddr_at {
