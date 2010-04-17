858e8d5a48372703b368ad9c19fa977f1a12c086
demon <demon@f17517cd-6fe8-449b-99a5-82cbd33fd24b>
2010-04-17 23:51:21 +0000

    A Perl clone of 'nbplkup', to allow me to verify the functioning of Net::Atalk::NBP.

---
+++ examples/nbplkup.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use diagnostics;
+
+use IO::Socket::DDP;
+use Net::Atalk;
+use Net::Atalk::NBP;
+use Getopt::Long;
+
+my $maxents;
+my $localaddr;
+
+sub usage {
+	print "Usage:\t", $0, " [ -A address ] [ -r responses] [ obj:type\@zone ]\n";
+	exit(1);
+}
+
+GetOptions('A=s' => \$localaddr,
+		   'r=i' => \$maxents) || usage();
+
+my ($type, $zone);
+
+my ($host) = @ARGV;
+if (defined $host) {
+	my ($type, $zone) = $host =~ s/(?::(\w+|=))?(?:\@(\w+|\*))?$//;
+	($type, $zone) = ($1, $2);
+}
+
+foreach my $tuple (NBPLookup($host, $type, $zone, $localaddr, $maxents)) {
+	printf("\%31s:\%-32s \%9s:\%-3d\n", @$tuple[3,4,0,1]);
+}
