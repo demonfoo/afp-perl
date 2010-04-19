f10967a26a7b0921aa4fcfb235dee4899079f3cc
demon <demon@f17517cd-6fe8-449b-99a5-82cbd33fd24b>
2010-04-19 23:08:23 +0000

    Implemented a fair portion of Net::Atalk::ATP. Incomplete.

--- Net/Atalk/ATP.pm
+++ Net/Atalk/ATP.pm
@@ -1,10 +1,16 @@
 package Net::Atalk::ATP;
 
+use strict;
+use warnings;
+
+use IO::Socket::DDP;
+use Net::Atalk;
+use Time::HiRes qw(gettimeofday);
+use IO::Poll qw(POLLIN);
+use IO::Handle;
 use threads;
 use threads::shared;
 use Thread::Semaphore;
-use strict;
-use warnings;
 
 # ATP message types.
 use constant ATP_TReq			=> (0x1 << 6);	# Transaction request
@@ -13,9 +19,12 @@ use constant ATP_TRel			=> (0x3 << 6);	# Transaction release
 
 # Fields of the control byte (first byte) in an ATP message.
 use constant ATP_CTL_FNCODE		=> 0xC0;
-use constant ATP_CTL_XOBIT		=> 0x20;
-use constant ATP_CTL_EOMBIT		=> 0x10;
-use constant ATP_CTL_STSBIT		=> 0x08;
+use constant ATP_CTL_XOBIT		=> 0x20;	# transaction must happen
+											# exactly once
+use constant ATP_CTL_EOMBIT		=> 0x10;	# packet is end of message
+use constant ATP_CTL_STSBIT		=> 0x08;	# send transaction status; upon
+											# receipt by originator, resend
+											# TReq packet
 use constant ATP_CTL_TREL_TMOUT	=> 0x07;
 
 # TRel timeout periods for XO (exactly-once) transactions. Ignored by
@@ -30,16 +39,295 @@ use constant ATP_TREL_8MIN		=> 0x04;
 # The maximum length of the ATP message body.
 use constant ATP_MAXLEN			=> 578;
 
-my $atp_header = 'CCnN';
-my @atp_header_fields = ('ctl', 'bmp_seq', 'tid', 'msglen');
+sub new {
+	my ($class, $host, $port) = @_;
 
-sub thread_proc {
-	my ($shared, $host, $socket) = @_;
+	my $obj = bless {}, $class;
 
-	my $conn;
+	my $shared = &share({});
+	%$shared = (
+				 'running'		=> 0,
+				 'exit'			=> 0,
+				 'last_txid'	=> 0,
+				 'conn_fd'		=> undef,
+				 'conn_sem'		=> new Thread::Semaphore(0),
+				 'TxCB_list'	=> &share({}),
+				 'RqCB_list'	=> &share({}),
+				 'RqCB_txidq'	=> &share([]),
+				 'RspCB_list'	=> &share({}),
+			   );
+	$$obj{'Shared'} = $shared;
+	my $thread = threads->create(\&thread_core, $shared, $host, $port
+	$$obj{'Dispatcher'} = $thread;
+	$$shared{'conn_sem'}->down();
+	$$obj{'Conn'} = new IO::Handle;
+	if ($$shared{'running'} == 1) {
+		$$obj{'Conn'}->fdopen($$shared{'conn_fd'}, 'w');
+	}
+	$$shared{'conn_sem'}->up();
 
-	while ($$shared{'exit'} == 0) {
-		if ($poll->poll(
+	return $obj;
 }
 
+# This function is the body of the thread. Similar to DSI, but I haven't
+# yet decided between fully-dispatched and hybrid dispatched models;
+# since the thread may have to resend the TReq packet an indeterminate
+# number of times, it might be better to just hand it off to it in the
+# first place. I'll see where things go.
+sub thread_core {
+	my ($shared, $host, $port) = @_;
 
+	# Set up the datagram socket to the target host. There's no connection
+	# status per-se, since DDP is datagram-oriented, not connection-oriented
+	# like TCP is.
+	my %connect_args = ( 'PeerAddr'		=> $host,
+						 'PeerPort'		=> $port,
+						 'Proto'		=> 'ddp',
+						 'Type'			=> SOCK_DGRAM );
+	my $conn = new IO::Socket::DDP(%connect_args);
+	$$shared{'running'} = 1;
+
+	# FIXME: any kind of session failure connection I can do from here?
+	# thinking no, but we'll see.
+	
+	$$shared{'conn_fd'} = fileno($conn);
+	$$shared{'running'} = 1;
+	$$shared{'sockaddr'} = $conn->sockaddr();
+	$$shared{'sockport'} = $conn->sockport();
+	$$shared{'peeraddr'} = $conn->peeraddr();
+	$$shared{'peerport'} = $conn->peerport();
+	$$shared{'sockdomain'} = AF_APPLETALK;
+	$$shared{'conn_sem'}->up();
+
+	# Set up a poll object for checking out our socket. Also preallocate
+	# several variables which will be used in the main loop.
+	my $poll = new IO::Poll();
+	$poll->mask($conn, POLLIN);
+	my ($id, $TxCB, $sec, $usec, $delta, $from, $msg, %msgdata, $msgtype,
+		$wants_sts, $is_eom, $seqno, $RqCB, $is_xo, $xo_tmout);
+
+	my $atp_header = 'CCCna[4]a*';
+	my @atp_header_fields = ('ddp_type', 'ctl', 'bmp_seq', 'tid', 'userbytes',
+			'payload');
+
+	my %xo_timeouts = (
+						&ATP_TREL_30SEC	=> 30,
+						&ATP_TREL_1MIN	=> 60,
+						&ATP_TREL_2MIN	=> 120,
+						&ATP_TREL_4MIN	=> 240,
+						&ATP_TREL_8MIN	=> 480,
+					  );
+
+MAINLOOP:
+	while ($$shared{'exit'} == 0) {
+		# Okay, now we need to check existing outbound transactions for
+		# status, resends, cleanups, etc...
+		print "thread: checking pending transaction list\n";
+		foreach $id (keys %{$$shared{'TxCB_list'}}) {
+			print "thread: txid ", $id, " pending\n";
+			$TxCB = $$shared{'TxCB_list'}{$id};
+			($sec, $usec) = gettimeofday();
+			$delta = ($sec - $$TxCB{'sec'}) +
+					(($usec - $$TxCB{'usec'}) / 1000000);
+			if ($delta >= $tmout) {
+				print "thread: transaction is past expire\n";
+				# okay, packet data needs to be resent; sequence mask
+				# will be updated in-place elsewhere, so just need to
+				# send again, decrement the retry counter, and update
+				# the start timer.
+				if ($$TxCB{'ntries'} > -1) {
+					print "thread: transaction still has tries left, resending for another shot...\n";
+					$$TxCB{'ntries'}--;
+					$$shared{'conn_sem'}->down();
+					send($conn, $$TxCB{'msg'}, 0);
+					@$TxCB{'sec', 'usec'} = ($sec, $usec); # close enough
+					$$shared{'conn_sem'}->up();
+				}
+				else {
+					print "thread: okay, transaction has no more tries, closing it out\n";
+					# Okay, you've had enough go-arounds. Time to put
+					# this dog down.
+					${$$TxCB{'sflag'}} = 0;
+					delete $$shared{'TxCB_list'}{$id};
+					$$TxCB{'sem'}->up();
+				}
+			}
+		}
+		if ($poll->poll(1)) {
+			$$shared{'conn_sem'}->down();
+			$from = recv($conn, $msg, DDP_MAXSZ, 0);
+			$$shared{'conn_sem'}->up();
+			next MAINLOOP unless defined $from;
+
+			@msgdata{@atp_header_fields} = unpack($atp_header, $msg);
+			unless ($msgdata{'ddp_type'} == DDPTYPE_ATP) {
+				print "thread: packet received, but type was not DDPTYPE_ATP, ignoring\n";
+				next MAINLOOP;
+			}
+			$msgtype = $msgdata{'ctl'} & ATP_CTL_FNCODE;
+			$id = $$msgdata{'tid'};
+			if ($msgtype == ATP_TReq) {
+				print "thread: received a transaction request\n";
+				$is_xo = $msgdata{'ctl'} & ATL_CTL_XOBIT;
+				$xo_tmout = $msgdata{'ctl'} & ATP_CTL_TREL_TMOUT;
+
+				if (exists $$shared{'RqCB_list'}{$id}) {
+					print "thread: transaction request already exists for txid ", $id, ", ignoring\n";
+					next MAINLOOP;
+				}
+				$RqCB = {
+						  'txid'		=> $id,
+						  'is_xo'		=> $is_xo,
+						  'xo_tmout'	=> $xo_timeouts{$xo_tmout},
+						  'seq_bmp'		=> $msgdata{'bmp_seq'},
+						  'userbytes'	=> $msgdata{'userbytes'},
+						  'payload'		=> $msgdata{'payload'},
+						  'sockaddr'	=> $from,
+						};
+				$$shared{'RqCB_list'}{$id} = $RqCB;
+				push(@{$$shared{'RqCB_txidq'}}, $id);
+				print "thread: set up request callback for transaction request with txid ", $id, "\n";
+			}
+			elsif ($msgtype == ATP_TResp) {
+				print "thread: received a transaction response packet, let's see who it belongs to\n";
+				unless (exists $$shared{'TxCB_list'}{$id}) {
+					print "thread: txid is ", $id, " but no corresponding TxCB was found, moving on\n";
+					next MAINLOOP;
+				}
+
+				$TxCB = $$shared{'TxCB_list'}{$id};
+				$is_eom = $msgdata{'ctl'} & ATP_CTL_EOMBIT;
+				$wants_sts = $msgdata{'ctl'} & ATP_CTL_STSBIT;
+				$seqno = $$msgdata{'bmp_seq'};
+
+				if ($is_eom) {
+					print "thread: server says this packet is last in sequence, fixing up seq bitmap\n"
+					$$TxCB{'seq_bmp'} &= 0xFF >> (7 - $seqno);
+				}
+				if ($wants_sts) {
+					print "thread: server wants us to send back transaction status\n";
+				}
+				if ($$TxCB{'seq_bmp'} & (1 << $seqno) {
+					print "thread: received packet with seq no ", $seqno, ", appears not to be a dup\n";
+					# put data into the array of stored payloads
+					$$TxCB{'payloads'}[$seqno] = $msgdata{'payload'};
+					# clear the bit in the sequence bitmap
+					$$TxCB{'seq_bmp'} &= ~(1 << $seqno) & 0xFF;
+					# update packet data with new sequence bitmap
+					substr($$TxCB{'msg'}, 2, 1, pack('C', $$TxCB{'seq_bmp'}));
+				}
+				else {
+					print "thread: received packet with seq no ", $seqno, ", already received, ignoring\n";
+				}
+
+				unless ($$TxCB{'seq_bmp'}) {
+					print "thread: okay, appears transaction is complete, indicating success and notifying caller\n";
+					# if this is the case, we've received everything we're
+					# expecting from the server side, so we've succeeded...
+					${$$TxCB{'sflag'}} = 1;
+					delete $$shared{'TxCB_list'}{$id};
+					$$TxCB{'sem'}->up();
+					# if it was an XO transaction, we should send a TRel here
+					if ($$TxCB{'is_xo'}) {
+						print "thread: transaction is XO, so will send TRel to server\n";
+						$$TxCB{'ctl_byte'} &= ~ATP_CTL_FNCODE & 0xFF;
+						$$TxCB{'ctl_byte'} |= ATP_TRel;
+						substr($$TxCB{'msg'}, 1, 1,
+								pack('C', $$TxCB{'ctl_byte'}));
+						$$shared{'conn_sem'}->down();
+						send($conn, $$TxCB{'msg'}, 0);
+						$$shared{'conn_sem'}->up();
+					}
+					next MAINLOOP;
+				}
+
+				# if the server wants an STS, or the sequence number is
+				# high enough that it's not going up further but there are
+				# still packets we need, then resend the request packet.
+				if ($wants_sts || ($seq_bmp && ($seq_bmp >> $seqno))) {
+					print "thread: resending request packet for STS or to satisfy missing chunks\n";
+					$$shared{'conn_sem'}->down();
+					send($conn, $$TxCB{'msg'}, 0);
+					@$TxCB{'sec', 'usec'} = gettimeofday();
+					$$shared{'conn_sem'}->up();
+				}
+			}
+			elsif ($msgtype == ATP_TRel) {
+				print "thread: received a transaction release\n";
+			}
+		}
+	}
+	$$shared{'running'} = -1;
+	undef $$shared{'conn_fd'};
+	close($conn);
+}
+
+sub SendTransaction {
+	my ($self, $is_xo, $data, $user_bytes, $rlen, $rdata_r, $tmout, $ntries,
+			$xo_tmout, $sflag_r) = @_;
+
+	# Check a few parameters before we proceed.
+	return if length($data) > ATP_MAXLEN;
+	return if $rlen > 8;
+	return if length($user_bytes) > 4;
+	die('$rdata_r must be a scalar ref')
+			unless ref($rdata_r) eq 'SCALAR' or ref($rdata_r) eq 'REF';
+	die('$sflag_r must be a scalar ref')
+			unless ref($sflag_r) eq 'SCALAR' or ref($sflag_r) eq 'REF';
+
+	# Set up the outgoing transaction request packet.
+	my $ctl_byte = ATP_TReq;
+	if ($is_xo) {
+		$ctl_byte |= ATP_CTL_XOBIT;
+		$ctl_byte |= $xo_tmout;
+	}
+	my $seq_bmp = 0xFF >> (8 - $rlen);
+	my $txid = ++$$self{'last_txid'};
+	my $msg = pack($atp_header, DDPTYPE_ATP, $ctl_byte, $seq_bmp, $txid,
+			$user_bytes, $data);
+
+	# Set up the transaction control block.
+	my $TxCB = {
+				 'msg'		=> $msg,
+				 'ntries'	=> $ntries - 1, # one less, since this is
+											# already try number one...
+				 'payloads'	=> &share([]),
+				 'ctl_byte'	=> $ctl_byte,
+				 'seq_bmp'	=> $seq_bmp,
+				 'is_xo'	=> $is_xo,
+				 'tmout'	=> $tmout,
+				 'sec'		=> undef,
+				 'usec'		=> undef,
+				 'sem'		=> new Thread::Semaphore(0),
+				 'sflag'	=> ,
+			   };
+	$$rdata_r = $$TxCB{'payloads'};
+	# Register our transaction control block so the thread can see it,
+	# since we have no idea how soon the response will come back from
+	# who we're talking to.
+	$$self{'Shared'}{'TxCB_list'}{$txid} = $TxCB;
+	print "SendTransaction(): Queued transaction block as txid ", $txid, "\n";
+
+	$$self{'Shared'}{'conn_sem'}->down();
+	send($$shared{'Conn'}, $msg, 0);
+	$$self{'Shared'}{'conn_sem'}->up();
+	print "SendTransaction(): Sent request packet to server\n";
+	# indicate this as when the transaction has started
+	@$TxCB{'start_sec', 'start_usec'} = gettimeofday();
+
+	return($txid, $$TxCB{'sem'});
+}
+
+sub GetTransaction {
+	my ($self) = @_;
+
+	unless (scalar(@{$$self{'Shared'}{'RqCB_txidq'}})) {
+		print "GetTransaction(): No unchecked incoming transactions to return, returning undef\n";
+		return undef;
+	}
+	my $txid = shift(@{$$self{'Shared'}{'RqCB_txidq'}});
+	print "GetTransaction(): Returning transaction request block for txid ", $txid, "\n";
+	return $$self{'Shared'}{'RqCB_list'}{$txid};
+}
+
+1;
