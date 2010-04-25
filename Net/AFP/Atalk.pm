c6a361f1ccee8abdaba2815991ac86e0dcffe811
demon <demon@f17517cd-6fe8-449b-99a5-82cbd33fd24b>
2010-04-25 07:33:32 +0000

    Rip more code from Net::AFP::TCP to make this (hopefully) work.

--- Net/AFP/Atalk.pm
+++ Net/AFP/Atalk.pm
@@ -2,18 +2,84 @@
 # for handling an AFP session over AppleTalk protocol.
 package Net::AFP::Atalk;
 use Net::AFP;
+use Net::AFP::Parsers;
+use Net::AFP::Result;
+use Net::Atalk::ASP;
+use Exporter;
+
 use strict;
+use warnings;
 
 our @ISA = qw(Net::AFP);
+our @EXPORT = qw(kFPShortName kFPLongName kFPUTF8Name kFPSoftCreate
+				 kFPHardCreate);
 
 sub new {
+	my ($class, $host, $port) = @_;
+	print 'called ', (caller(0))[3], "\n" if defined $::__AFP_DEBUG;
+	my $obj = {};
+	bless $obj, $class;
+
+	$$obj{'ASPSession'} = new Net::Atalk::ASP($host, $port);
+	my $rc = $$obj{'ASPSession'}->SPOpenSession();
+	# This will cause the client code to send an SPTickle, and resend
+	# it every 30 seconds, forever. The server never actually sends
+	# back a "response" to the pending transaction, thus forcing the
+	# tickle request to keep going automatically, with no extra additions
+	# required to the thread.
+	$$obj{'ASPSession'}->SPTickle(30, -1);
+	return $rc unless $rc == SPNoError;
+	return $obj;
+}
+
+
+sub close {
+	my ($self) = @_;
+	print 'called ', (caller(0))[3], "\n" if defined $::__AFP_DEBUG;
+
+	$$self{'ASPSession'}->SPCloseSession();
+	$$self{'ASPSession'}->close();
+}
+
+sub CheckAttnQueue {
+	my ($self) = @_;
+
+	print '', (caller(0))[3], ": stub\n";
 }
 
 sub SendAFPMessage {
+	my ($self, $payload, $resp_r) = @_;
 
+	print 'called ', (caller(0))[3], "\n" if defined $::__AFP_DEBUG;
+	$self->CheckAttnQueue();
+	return $$self{'ASPSession'}->SPCommand($payload, $resp_r);
 }
 
-sub GetAFPReply {
+sub SendAFPWrite { # {{{1
+	my ($self, $payload, $data_r, $resp_r) = @_;
+	
+	print 'called ', (caller(0))[3], "\n" if defined $::__AFP_DEBUG;
+	$self->CheckAttnQueue();
+	return $$self{'ASPSession'}->SPWrite($payload, $data_r, $resp_r);
+} # }}}1
 
-}
+sub GetStatus { # {{{1
+	my ($class, $host, $port, $resp_r) = @_;
+	if (ref($class)) {
+		die('GetStatus() should NEVER be called against an active object');
+		return -1;
+	}
 
+	print 'called ', (caller(0))[3], "\n" if defined $::__AFP_DEBUG;
+	my $obj = new Net::Atalk::ASP($host, $port);
+	my $resp;
+	my $rc = $obj->SPGetStatus(\$resp);
+	$obj->close();
+	return $rc unless $rc == SPNoError;
+
+	$$resp_r = _ParseSrvrInfo($resp);
+	return $rc;
+} # }}}1
+
+1;
+# vim: ts=4 ai fdm=marker
