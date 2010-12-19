be2e0fa619c7e4d18f3cd40c9db496a535959637
demon <demon@f17517cd-6fe8-449b-99a5-82cbd33fd24b>
2010-12-19 18:29:33 +0000

    Prepare to cut the 0.62.1 tag.

--- lib/Net/AFP.pm
+++ lib/Net/AFP.pm
@@ -26,7 +26,7 @@ our @EXPORT = qw(kFPShortName kFPLongName kFPUTF8Name kFPSoftCreate
                  kFPHardCreate kFPStartEndFlag kFPLockUnlockFlag);
 
 # define constants {{{1
-our $VERSION = '0.62';
+our $VERSION = '0.62.1';
 use constant kFPByteRangeLock           => 1;   # AFP 2.0
 use constant kFPCloseVol                => 2;   # AFP 2.0
 use constant kFPCloseDir                => 3;   # AFP 2.0
