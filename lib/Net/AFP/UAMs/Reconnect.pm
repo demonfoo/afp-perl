47e7c6a9707e9b4013992afb352fa58e36050eb2
demon <demon@f17517cd-6fe8-449b-99a5-82cbd33fd24b>
2011-01-25 06:29:08 +0000

    Added more implementation of the Reconnect UAM; still gotta figure a few more things out.

--- lib/Net/AFP/UAMs/Reconnect.pm
+++ lib/Net/AFP/UAMs/Reconnect.pm
@@ -1,46 +1,70 @@
+# This UAM was added as of AFP 3.1.
 
 package Net::AFP::UAMs::Reconnect;
 
 use Net::AFP::Parsers;
 use Net::AFP::TokenTypes;
+use Digest::MD5 qw(md5);
 use strict;
 use warnings;
 
 use constant UAMNAME => 'Recon1';
 
+use constant C2SIV => 'WOMDMOAB';
+use constant S2CIV => 'WOMDMOAB';
+
 sub GetCred {
-	my($session) = @_;
+    my($session) = @_;
 
-	my $ctx = $session->{'cryptctx'};
-	unless (defined $ctx) {
-		print "No encryption context was stored; must use DHCAST128/DHX2 for Recon1!\n";
-		return undef;
-	}
+    my $ctx = $session->{'cryptctx'};
+    unless (defined $ctx) {
+        print "No encryption context was stored; must use DHCAST128/DHX2 for Recon1!\n";
+        return undef;
+    }
 
-	my $stamp = time() - $Net::AFP::Parsers::globalTimeOffset;
-	my $t1_k1 = $ctx->encrypt(pack('N', $stamp));
-	my $resp = '';
-	my $ID = '';  # need to actually do something with this
-	my $rc = $session->FPGetSessionToken(Net::AFP::TokenTypes::kRecon1Login,
-			$t1_k1, $ID, \$resp);
-	if ($rc != Net::AFP::Result::kFPNoErr) {
-		print "Calling FPGetSessionToken failed\n";
-		return undef;
-	}
+    my $stamp = time() - globalTimeOffset;
+    my $ID = '';  # need to actually do something with this
+    my $t1_k1 = $ctx->encrypt(pack('Na*', $stamp, $ID));
+    my $resp = '';
+    my $rc = $session->FPGetSessionToken(kRecon1Login, undef, $t1_k1, \$resp);
+    if ($rc != Net::AFP::Result::kFPNoErr) {
+        print "Calling FPGetSessionToken failed\n";
+        return undef;
+    }
 
-	my $token = $ctx->decrypt($resp);
-	print "decrypted token data is: ", unpack('H*', $token), "\n";
+    my $token = $ctx->decrypt($resp);
+    print "decrypted token data is: ", unpack('H*', $token), "\n";
 
-	return 1;
+    return 1;
 }
 
 sub RefreshCred {
-	my($session) = @_;
+    my($session) = @_;
 
+    my $ctx = $session->{'cryptctx'};
+    unless (defined $ctx) {
+        print "No encryption context was stored; must use DHCAST128/DHX2 for Recon1!\n";
+        return undef;
+    }
+
+    my $stamp = time() - globalTimeOffset;
+    my $ID = '';  # need to actually do something with this
+    $ctx->iv(C2SIV);
+    my $t1_k1 = $ctx->encrypt(pack('Na*', $stamp, $ID));
+    my $resp ;
+    my $rc = $session->FPGetSessionToken(kRecon1RefreshToken, undef, $t1_k1, \$resp);
+    if ($rc != Net::AFP::Result::kFPNoErr) {
+        print "Calling FPGetSessionToken failed\n";
+        return undef;
+    }
+    #$ctx->iv(S2CIV);
+    #my $plaintext = $ctx->decrypt($resp);
+    #my($s, $m, $exp, $t3, $validator) = unpack('a[8]NNNa*', $plaintext);
+    return($resp);
 }
 
 sub Reconnect {
-	my($session, $AFPVersion, $username, $VolID, $Token) = @_;
+    my($session, $AFPVersion, $username, $VolID, $Token) = @_;
 
 
 }
