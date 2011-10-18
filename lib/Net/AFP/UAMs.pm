11ca98ac6b20ac325c06b6f08a15c11844961ca1
demon <demon@f17517cd-6fe8-449b-99a5-82cbd33fd24b>
2011-10-18 14:30:53 +0000

    Clean up tabbing.

--- lib/Net/AFP/UAMs.pm
+++ lib/Net/AFP/UAMs.pm
@@ -28,15 +28,15 @@ my @UAMReg = ();
 # client. a preference of less than 0 means the UAM will never be selected
 # automatically.
 sub RegisterUAM {
-	my ($uamname, $class, $pref) = @_;
+    my ($uamname, $class, $pref) = @_;
 
-	my $uaminfo = { 'name' => $uamname, 'class' => $class, 'pref' => $pref };
-	for (my $i = 0; $i <= $#UAMReg; $i++) {
-		next if $UAMReg[$i]->{'pref'} > $pref;
-		@UAMReg = (@UAMReg[0 .. ($i - 1)], $uaminfo, @UAMReg[$i .. $#UAMReg]);
-		return;
-	}
-	push(@UAMReg, $uaminfo);
+    my $uaminfo = { 'name' => $uamname, 'class' => $class, 'pref' => $pref };
+    for (my $i = 0; $i <= $#UAMReg; $i++) {
+        next if $UAMReg[$i]->{'pref'} > $pref;
+        @UAMReg = (@UAMReg[0 .. ($i - 1)], $uaminfo, @UAMReg[$i .. $#UAMReg]);
+        return;
+    }
+    push(@UAMReg, $uaminfo);
 }
 
 # Where am I being included from? Use our own package name to get the
@@ -56,13 +56,13 @@ if (-d $incpath) {
 # Try including each of them via eval, so that if they explode, it won't
 # impair our ability to continue on.
 foreach my $uampath (@uampaths) {
-	eval { require $uampath; };
-#	if ($@) {
-#		print STDERR 'Couldn\'t include ', $uampath, ":\n";
-#		print STDERR '-' x 15, ' start error text ', '-' x 15, "\n", $@;
-#		print STDERR '-' x 16, ' end error text ', '-' x 16, "\n";
-#		print STDERR "This error is not fatal; other UAMs will be tried.\n\n";
-#	}
+    eval { require $uampath; };
+#    if ($@) {
+#        print STDERR 'Couldn\'t include ', $uampath, ":\n";
+#        print STDERR '-' x 15, ' start error text ', '-' x 15, "\n", $@;
+#        print STDERR '-' x 16, ' end error text ', '-' x 16, "\n";
+#        print STDERR "This error is not fatal; other UAMs will be tried.\n\n";
+#    }
 }
 
 =head1 FUNCTIONS
@@ -90,12 +90,12 @@ L<Net::AFP::Versions/GetPreferredVersion()> for more information.
 
 =cut
 sub GuestAuth($$) {
-	my($session, $AFPVersion) = @_;
-	my $rc = Net::AFP::UAMs::Anonymous::Authenticate($session, $AFPVersion);
-	if ($rc == kFPNoErr) {
-		$$session{'AFPVersion'} = $AFPVersion;
-	}
-	return $rc;
+    my($session, $AFPVersion) = @_;
+    my $rc = Net::AFP::UAMs::Anonymous::Authenticate($session, $AFPVersion);
+    if ($rc == kFPNoErr) {
+        $session->{'AFPVersion'} = $AFPVersion;
+    }
+    return $rc;
 }
 
 =item PasswordAuth()
@@ -133,32 +133,32 @@ A subroutine reference which is to be used to acquire the user's password.
 
 =cut
 sub PasswordAuth($$$$$) {
-	my($session, $AFPVersion, $SupportedUAMs, $UserName, $PwCallback) = @_;
-#	die('Need a function ref for password callback')
-#			unless ref($PwCallback) eq 'CODE';
+    my($session, $AFPVersion, $SupportedUAMs, $UserName, $PwCallback) = @_;
+#    die('Need a function ref for password callback')
+#            unless ref($PwCallback) eq 'CODE';
 
-	# The AFP server often sets a really lousy UAM order; I should really
-	# have a prioritized list of UAMs that we know are good, and try to
-	# use the best ones first. OS 9.x likes to specify cleartext auth at
-	# the top of the list... not so great.
-	my %ReqUAMs = map { lc($_), 1 } @$SupportedUAMs;
-	foreach my $uaminfo (@UAMReg) {
-		next unless exists $ReqUAMs{lc($$uaminfo{'name'})};
-		last if $$uaminfo{'pref'} < 0 and scalar(keys %ReqUAMs) > 1;
-		my $function = $$uaminfo{'class'} . '::Authenticate';
-		DEBUG('auth function is ', $function);
-		$session->{'username'} = $UserName;
-		my $rc = &{$function}($session, $AFPVersion, $UserName, $PwCallback);
-		if ($rc == kFPNoErr) {
-			$$session{'AFPVersion'} = $AFPVersion;
-		}
-		return $rc;
-	}
+    # The AFP server often sets a really lousy UAM order; I should really
+    # have a prioritized list of UAMs that we know are good, and try to
+    # use the best ones first. OS 9.x likes to specify cleartext auth at
+    # the top of the list... not so great.
+    my %ReqUAMs = map { lc($_), 1 } @$SupportedUAMs;
+    foreach my $uaminfo (@UAMReg) {
+        next unless exists $ReqUAMs{lc($$uaminfo{'name'})};
+        last if $uaminfo->{'pref'} < 0 and scalar(keys %ReqUAMs) > 1;
+        my $function = $$uaminfo{'class'} . '::Authenticate';
+        DEBUG('auth function is ', $function);
+        $session->{'username'} = $UserName;
+        my $rc = &{$function}($session, $AFPVersion, $UserName, $PwCallback);
+        if ($rc == kFPNoErr) {
+            $session->{'AFPVersion'} = $AFPVersion;
+        }
+        return $rc;
+    }
 
-	# If we reach this point, none of the UAMs the server knew were available.
-	DEBUG((caller(0))[3], 
-			" Could not find an agreeable UAM for authenticating to server\n");
-	return kFPBadUAM;
+    # If we reach this point, none of the UAMs the server knew were available.
+    DEBUG((caller(0))[3], 
+            " Could not find an agreeable UAM for authenticating to server\n");
+    return kFPBadUAM;
 }
 
 =item ChangePassword()
@@ -195,21 +195,21 @@ A string containing the new password to be set for the user.
 sub ChangePassword {
     my($session, $SupportedUAMs, $UserName, $OldPW, $NewPW) = @_;
 
-	my %ReqUAMs = map { lc($_), 1 } @$SupportedUAMs;
-	foreach my $uaminfo (@UAMReg) {
-		next unless exists $ReqUAMs{lc($$uaminfo{'name'})};
-		last if $$uaminfo{'pref'} < 0 and scalar(keys %ReqUAMs) > 1;
-		my $function = $$uaminfo{'class'} . '::ChangePassword';
-		DEBUG('password changing function is ', $function);
+    my %ReqUAMs = map { lc($_), 1 } @$SupportedUAMs;
+    foreach my $uaminfo (@UAMReg) {
+        next unless exists $ReqUAMs{lc($$uaminfo{'name'})};
+        last if $$uaminfo{'pref'} < 0 and scalar(keys %ReqUAMs) > 1;
+        my $function = $$uaminfo{'class'} . '::ChangePassword';
+        DEBUG('password changing function is ', $function);
         #my $NewPW = &$PwCallback();
-		my $rc = &{$function}($session, $UserName, $OldPW, $NewPW);
-		return $rc;
-	}
+        my $rc = &{$function}($session, $UserName, $OldPW, $NewPW);
+        return $rc;
+    }
 
-	# If we reach this point, none of the UAMs the server knew were available.
-	DEBUG((caller(0))[3], 
-			" Could not find valid password changing UAM\n");
-	return kFPBadUAM;
+    # If we reach this point, none of the UAMs the server knew were available.
+    DEBUG((caller(0))[3], 
+            " Could not find valid password changing UAM\n");
+    return kFPBadUAM;
 }
 
 =back
