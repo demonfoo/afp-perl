9911a47543a9a8ca65ffcf09a340db8a5e3a3755
Derrik Pates <demon@now.ai>
2012-10-09 20:52:43 -0600

    Added informational comments.

--- lib/Net/AFP/DirAttrs.pm
+++ lib/Net/AFP/DirAttrs.pm
@@ -2,13 +2,13 @@ package Net::AFP::DirAttrs;
 
 use constant kFPInvisibleBit        => 0x0001;
 use constant kAttrIsExpFolderBit    => 0x0002;  # AFP 2.1
-use constant kFPSystemBit           => 0x0004;
+use constant kFPSystemBit           => 0x0004;  # AFP 2.0
 use constant kAttrMountedBit        => 0x0008;  # AFP 2.1
 use constant kAttrInExpFolderBit    => 0x0010;  # AFP 2.1
-use constant kFPBackUpNeededBit     => 0x0040;
-use constant kFPRenameInhibitBit    => 0x0080;
-use constant kFPDeleteInhibitBit    => 0x0100;
+use constant kFPBackUpNeededBit     => 0x0040;  # AFP 2.0
+use constant kFPRenameInhibitBit    => 0x0080;  # AFP 2.0
+use constant kFPDeleteInhibitBit    => 0x0100;  # AFP 2.0
 use constant kFPSetClearBit         => 0x8000;
 
 1;
-# vim: ts=4
+# vim: ts=4 et ai
--- lib/Net/AFP/FileAttrs.pm
+++ lib/Net/AFP/FileAttrs.pm
@@ -2,15 +2,15 @@ package Net::AFP::FileAttrs;
 
 use constant kFPInvisibleBit        => 0x0001;
 use constant kFPMultiUserBit        => 0x0002;
-use constant kFPSystemBit           => 0x0004;
+use constant kFPSystemBit           => 0x0004;  # AFP 2.0
 use constant kFPDAlreadyOpenBit     => 0x0008;
 use constant kFPRAlreadyOpenBit     => 0x0010;
-use constant kFPWriteInhibitBit     => 0x0020;
-use constant kFPBackUpNeededBit     => 0x0040;
-use constant kFPRenameInhibitBit    => 0x0080;
-use constant kFPDeleteInhibitBit    => 0x0100;
-use constant kFPCopyProtectBit      => 0x0400;
+use constant kFPWriteInhibitBit     => 0x0020;  # Was "ReadOnly" before AFP 2.0
+use constant kFPBackUpNeededBit     => 0x0040;  # AFP 2.0
+use constant kFPRenameInhibitBit    => 0x0080;  # AFP 2.0
+use constant kFPDeleteInhibitBit    => 0x0100;  # AFP 2.0
+use constant kFPCopyProtectBit      => 0x0400;  # AFP 2.0
 use constant kFPSetClearBit         => 0x8000;
 
 1;
-# vim: ts=4
+# vim: ts=4 et ai
