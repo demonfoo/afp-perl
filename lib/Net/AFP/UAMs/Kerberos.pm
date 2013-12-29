b29d1f446ad80e2287983be5b3a7b3c91d100277
Derrik Pates <demon@now.ai>
2013-12-28 20:17:27 -0700

    Changes to the Kerberos UAM, which should make it more likely to work (but still needs testing).

--- lib/Net/AFP/UAMs/Kerberos.pm
+++ lib/Net/AFP/UAMs/Kerberos.pm
@@ -6,6 +6,7 @@ use constant UAMNAME => 'Client Krb v2';
 
 use GSSAPI;
 use Net::AFP::Result;
+use Net::AFP::TokenTypes;
 use Net::AFP;
 
 use strict;
@@ -13,56 +14,69 @@ use strict;
 #Net::AFP::UAMs::RegisterUAM(UAMNAME, __PACKAGE__, 300);
 
 sub Authenticate {
-	my ($session, $AFPVersion, $username, $pw_cb, $realm) = @_;
+    my ($session, $AFPVersion, $username, $pw_cb, $realm) = @_;
 
-	# Ensure that we've been handed an appropriate object.
-	die('Object MUST be of type Net::AFP!')
-			unless ref($session) and $session->isa('Net::AFP');
+    # Ensure that we've been handed an appropriate object.
+    die('Object MUST be of type Net::AFP!')
+            unless ref($session) and $session->isa('Net::AFP');
 
-	die('Password callback MUST be a subroutine ref')
-			unless ref($pw_cb) eq 'CODE';
+    die('Password callback MUST be a subroutine ref')
+            unless ref($pw_cb) eq 'CODE';
 
-	# FIXME: I think we're supposed to try to get the Kerberos ticket before
-	# we attempt to do FPLoginExt()...
+    # Try to get a Kerberos token, if we can...?
+    my $ctx;
+    my $gss_input_token = q();
+    my $status = GSSAPI::Context::init($ctx, GSS_C_NO_CREDENTIAL,
+            $realm, GSS_C_NO_OID, GSS_C_MUTUAL_FLAG | GSS_C_REPLAY_FLAG,
+            0, GSS_C_NO_CHANNEL_BINDINGS, $gss_input_token, my $out_mech,
+            my $gss_output_token, my $out_flags, my $out_time);
 
-	my $resp = '';
-	my $rc = $session->FPLoginExt(0, $AFPVersion, UAMNAME, kFPUTF8Name,
-			$username, kFPUTF8Name, $realm, undef, \$resp);
-	print 'FPLoginExt() completed with result code ', $rc, "\n"
-			if defined $::__AFP_DEBUG;
+    $status || gss_exit('CLIENT::Unable to initialize security context', $status);
 
-	return $rc unless $rc == kFPAuthContinue;
+    # Assuming that succeeded, now we do the first stage of the login process.
+    my $resp = '';
+    my $rc = $session->FPLoginExt(0, $AFPVersion, UAMNAME, kFPUTF8Name,
+            $username, kFPUTF8Name, $realm, undef, \$resp);
+    print 'FPLoginExt() completed with result code ', $rc, "\n"
+            if defined $::__AFP_DEBUG;
 
-	my $ctx;
-	my $gss_input_token = q();
-	my $status = GSSAPI::Context::init($ctx, GSS_C_NO_CREDENTIAL,
-			$realm, GSS_C_NO_OID, GSS_C_MUTUAL_FLAG | GSS_C_REPLAY_FLAG,
-			0, GSS_C_NO_CHANNEL_BINDINGS, $gss_input_token, my $out_mech,
-			my $gss_output_token, my $out_flags, my $out_time);
+    return $rc unless $rc == kFPAuthContinue;
 
-	$status || gss_exit("CLIENT::Unable to initialize security context", $status);
+    # Now send the Kerberos ticket to the AFP server to be authorized...?
+    my $message = pack('C/a*x![s]S>/a*', $username, $gss_output_token);
+    my $sresp;
+    $rc = $session->FPLoginCont($resp->{ID}, $message, \$sresp);
 
-	my $message = pack('C/a*x![s]n/a*', $username, $gss_output_token);
-	my $sresp;
-	$rc = $session->FPLoginCont($$resp{'ID'}, $message, \$sresp);
+    return $rc unless $rc == kFPNoErr;
 
-	return $rc unless $rc == kFPNoErr;
-
-	($gss_input_token) = unpack('n/a*', $$sresp{'UserAuthInfo'});
-
-	$status = GSSAPI::Context::init($ctx, GSS_C_NO_CREDENTIAL,
-			$realm, GSS_C_NO_OID, GSS_C_MUTUAL_FLAG | GSS_C_REPLAY_FLAG,
-			0, GSS_C_NO_CHANNEL_BINDINGS, $gss_input_token, $out_mech,
-			$gss_output_token, $out_flags, $out_time);
-
-	$status || gss_exit("CLIENT::Unable to initialize security context", $status);
-
-	# FIXME: Apple's docs talk about using FPGetSessionToken() about
-	# here to get a random session key from the server, and also something
-	# about a disconnect token; I'm not really clear here on what I'm
-	# supposed to do with all that.
-	return $rc;
+    # Get an encrypted Kerberos session key from the AFP server.
+    my $enc_session_key;
+    # FIXME: What should the ID field contain? Docs don't really say...
+    $rc = $session->FPGetSessionToken(kGetKerberosSessionKey, 0, q{},
+            \$enc_session_key);
+    $status = $ctx->unwrap($enc_session_key, my $session_key,
+            my $conf_state, my $qop);
+    # FIXME: Okay, should have the session key in $session_key! Now, what
+    # the fuck do we do with it...
+    return $rc;
 
 }
-die('Incomplete code, should not be used yet');
+
+sub gss_exit {
+    my $errmsg = shift;
+    my $status = shift;
+
+    my @major_errors = $status->generic_message();
+    my @minor_errors = $status->specific_message();
+
+    print STDERR "$errmsg:\n";
+    foreach my $s (@major_errors) {
+        print STDERR "  MAJOR::$s\n";
+    }
+    foreach my $s (@minor_errors) {
+        print STDERR "  MINOR::$s\n";
+    }
+    return 1;
+}
+
 1;
