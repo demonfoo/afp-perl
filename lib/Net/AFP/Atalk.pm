48416deeca5d3105ccde88ad9f597bda23d76241
Derrik Pates <dpates@sugarcrm.com>
2024-11-08 10:18:26 -0700

    Move CheckAttnQueue to common class, since it's the same (enough).

--- lib/Net/AFP/Atalk.pm
+++ lib/Net/AFP/Atalk.pm
@@ -52,55 +52,6 @@ sub close { # {{{1
     return;
 } # }}}1
 
-sub CheckAttnQueue { # {{{1
-    my ($self) = @_;
-    $self->{logger}->debug(sub { sprintf q{called %s()}, (caller 3)[3] });
-
-    my $attnq = $self->{Session}{Shared}{attnq};
-    my $vol_update_checked;
-    while (my $msg = shift @{$attnq}) {
-        if ($msg & 0x8_000) {    # server says it's shutting down
-            $self->{logger}->info(sub { sprintf q{%s(): Received notification } .
-              q{of server intent to shut down}, (caller 3)[3] });
-            $self->{logger}->info(sub { sprintf q{%s(): Shutdown in %d minutes},
-              (caller 3)[3], ($msg & 0xFFF) });
-            if ($msg & 0x2_000) { # server also has a message for us
-                my $MsgData;
-                $self->FPGetSrvrMsg(1, 3, \$MsgData);
-                if ($MsgData->{ServerMessage} ne q{}) {
-                    $self->{logger}->info(sub { sprintf q{%s(): Shut down } .
-                      q{message: "%s"}, (caller 3)[3], $MsgData->{ServerMessage} });
-                }
-            }
-        }
-        elsif ($msg & 0x4_000) { # server says it's crashing
-            $self->{logger}->info(sub { sprintf q{%s(): Received notification } .
-                q{server is crashing; should really attempt reconnection, I } .
-                q{suppose...}, (caller 3)[3] });
-        }
-        elsif ($msg & 0x2_000) { # server message?
-            if ($msg & 0x1_000) { # server notification
-                if ($msg & 0x1) {
-                    next if $vol_update_checked;
-                    $self->{logger}->info(sub { sprintf q{%s(): ModDate } .
-                        q{updated on an attached volume, should do } .
-                        q{FPGetVolParms() to recheck}, (caller 3)[3] });
-                    $vol_update_checked = 1;
-                }
-            }
-            else { # server message
-                my $MsgData;
-                $self->FPGetSrvrMsg(1, 3, \$MsgData);
-                if ($MsgData->{ServerMessage} ne q{}) {
-                    $self->{logger}->info(sub { sprintf q{%s(): Server message: "%s"},
-                      (caller 3)[3], $MsgData->{ServerMessage} });
-                }
-            }
-        }
-    }
-    return;
-} # }}}1
-
 # This is a virtual method which is not for public consumption. Only
 # Net::AFP methods should ever call this.
 sub SendAFPMessage { # {{{1
